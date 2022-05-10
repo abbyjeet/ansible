c2689329cdb1225df3299a101035ff06e80e10da
Abhijit J <abbyjeet@gmail.com>
2022-05-10 18:00:23 +1000

    ansible with docker as remote node and roles using galaxy

---
+++ docker-playbook/README.md
@@ -0,0 +1,3 @@
+Use ansible-galaxy to create roles structure
+
+Source: https://ayeshasilvia.medium.com/testing-ansible-playbook-in-a-docker-container-21628e9ee256
---
+++ docker-playbook/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOCKER_CONTAINER_NAME="docker-test"
+
+cd docker && docker build -t myubuntu .
+
+docker run -ti --privileged --name $DOCKER_CONTAINER_NAME -d -p 5000:22 myubuntu
+
+cd ../ansible && ansible-playbook -i env/local_docker playbook.yml -vvv
+
+# docker stop $DOCKER_CONTAINER_NAME
+
+# docker rm $DOCKER_CONTAINER_NAME
